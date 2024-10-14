7b5895057194e170f3e4c33390a63599e91a5658
Bojjan <116097397+Bojjjan@users.noreply.github.com>
2024-10-14 18:12:04 +0200

    Update SysCare.bat menu options and add restart services functionality

--- SysCare.bat
+++ SysCare.bat
@@ -18,20 +18,57 @@ echo       \___ \ )  / \___ \( (__ /    \ )   / ) _)
 echo       (____/(__/  (____/ \___)\_/\_/(__\_)(____)
 echo.
 echo ========================================================
+echo                "ctrl + c" to end a task
+echo ========================================================
+echo.
 echo.
 echo [1] Check for application updates (winget)
 echo [2] Check System Health
-echo [3] Old Windows Context Menus
-echo [4] Exit
+echo [3] Restart apps/ Services
 echo.
+echo.
+echo [0] Exit
 echo ========================================================
-choice /c 1234 /n /m "Select an option: "
+choice /c 1230 /n /m "Select an option: "
 
 rem Errorlevel values handled below:
 if %errorlevel%==1 goto check_applications
 if %errorlevel%==2 goto systemHealthMenu
+if %errorlevel%==3 goto restartServices
 if %errorlevel%==4 goto exit
 
+:restartServices
+cls
+echo        ____  _  _  ____   ___   __   ____  ____ 
+echo       / ___)( \/ )/ ___) / __) / _\ (  _ \(  __)
+echo       \___ \ )  / \___ \( (__ /    \ )   / ) _) 
+echo       (____/(__/  (____/ \___)\_/\_/(__\_)(____)
+echo.
+echo ========================================================
+echo               RESTART APPS/ SERVICES
+echo ========================================================
+echo.
+echo [1] Rebuild Search index (windows Search)
+echo.
+echo.
+echo [0] Back to main menu
+echo ========================================================
+choice /c 012 /n /m "Select an option: "
+
+if %errorlevel%==1 goto main     			 :: '0'
+if %errorlevel%==2 goto RebuildWindowsSearch             :: '1'
+
+
+:RebuildWindowsSearch
+cls
+echo Restarting Windows Search service...
+
+net stop "WSearch"
+net start "WSearch"
+
+echo Windows Search service restarted successfully!
+pause
+goto restartServices
 
 :systemHealthMenu
 cls
@@ -45,17 +82,61 @@ echo                  SYSTEM HEALTH CHECK
 echo ========================================================
 echo [a] Full Health Check (takes a while)
 echo.
+echo ---------------- System Repair -----------------------
 echo [1] Restore system health (DISM /RestoreHealth)
 echo [2] Check the integrity of Windows system (sfc)
-echo [3] Back to main menu
+echo [3] Scan your disk for errors and repair them (chkdsk)
 echo.
+echo.
+
+echo ---------------- Clean System ------------------------
+echo [4] Disk Cleanup (cleanmgr)
+echo [5] Clean DNS (flushDNS)
+echo.
+
+echo.
+echo [0] Back to main menu
 echo ========================================================
-choice /c a123 /n /m "Select an option: "
+choice /c a1234507 /n /m "Select an option: "
 
 if %errorlevel%==1 goto all_System_health     :: 'a'
 if %errorlevel%==2 goto dismcheck             :: '1'
 if %errorlevel%==3 goto sfcscan               :: '2'
-if %errorlevel%==4 goto main                  :: '3'
+if %errorlevel%==4 goto chkdsk                :: '3'
+if %errorlevel%==5 goto cleanmgr              :: '4'
+if %errorlevel%==6 goto clearDNS              :: '5'
+if %errorlevel%==7 goto main                  :: '0'
+if %errorlevel%==8 goto RebuildSearchIndex    :: '7'
+
+
+:RebuildSearchIndex
+cls
+echo Rebuilding search index...
+
+powershell -Command "Stop-Service -Name WSearch"
+powershell -Command "Start-Service -Name WSearch"
+
+echo Search index rebuild started!
+pause
+goto systemHealthMenu
+
+:clearDNS
+cls
+echo Clearing DNS cache...
+ipconfig /flushdns
+echo DNS cache cleared!
+pause
+goto systemHealthMenu
+
+
+:cleanmgr
+cls
+echo Running Disk Cleanup...
+cleanmgr /sagerun:1
+Dism.exe /online /Cleanup-Image /StartComponentCleanup
+echo Disk Cleanup complete!
+pause
+goto systemHealthMenu
 
 :sfcscan
 cls
@@ -70,8 +151,10 @@ if %errorlevel% equ 0 (
 ) else (
     echo An unexpected error occurred during SFC scan. Check the CBS log for more information.
 )
+echo.
 pause
-exit /b
+goto systemHealthMenu
+
 
 
 :dismcheck
@@ -83,8 +166,25 @@ if %errorlevel% equ 0 (
 ) else (
     echo DISM encountered an error. Check the DISM log file at C:\Windows\Logs\DISM\dism.log for more information.
 )
+echo.
 pause
-exit /b
+goto systemHealthMenu
+
+:chkdsk
+cls
+chkdsk C: /f /r
+
+if %errorlevel% equ 0 (
+    echo No errors found on the disk.
+) else if %errorlevel% geq 1 (
+    echo CHKDSK found and corrected errors on the disk.
+) else (
+    echo CHKDSK encountered an error. Review the output or check the Event Viewer logs for details.
+)
+echo.
+pause
+goto systemHealthMenu
+
 
 
 :all_System_health
